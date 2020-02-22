4a04fa6f99eff63341ad64a72cc10469d264b4d6
Colman Davenport <colmandavenport@gmail.com>
2020-02-22 14:16:42 -0500

    wheee

--- src/main/java/blackrusemod/cards/ShatteredReality.java
+++ src/main/java/blackrusemod/cards/ShatteredReality.java
@@ -1,7 +1,7 @@
 package blackrusemod.cards;
 
 import com.badlogic.gdx.math.MathUtils;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
@@ -34,27 +34,28 @@ public class ShatteredReality extends AbstractShiftCard {
 		this.baseDamage = ATTACK_DMG;
 	}
 
+	@Override
 	public void use(AbstractPlayer p, AbstractMonster m) {
-		AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn),
-				AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+		addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AttackEffect.SLASH_DIAGONAL));
 	}
 	
+	@Override
 	public void triggerShift() {
-		AbstractDungeon.actionManager.addToBottom(new SFXAction("THUNDERCLAP", 0.05F));
+		addToBot(new SFXAction("THUNDERCLAP", 0.05F));
 		for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
 			if (!mo.isDeadOrEscaped()) {
-				AbstractDungeon.actionManager.addToBottom(new VFXAction(new LightningEffect(mo.drawX, mo.drawY), 0.05F));
-				AbstractDungeon.actionManager.addToBottom(new DamageAction(mo, new DamageInfo(AbstractDungeon.player,
-						damageCalculation(AbstractDungeon.player, mo, this.baseDamage)*2, this.damageTypeForTurn),
-						AbstractGameAction.AttackEffect.NONE));
+				addToBot(new VFXAction(new LightningEffect(mo.drawX, mo.drawY), 0.05F));
+				addToBot(new DamageAction(mo, new DamageInfo(AbstractDungeon.player, damageCalculation(AbstractDungeon.player, mo, this.baseDamage)*2, this.damageTypeForTurn), AttackEffect.NONE));
 			}
 		}
 	}
 
+	@Override
 	public AbstractCard makeCopy() {
 		return new ShatteredReality();
 	}
 
+	@Override
 	public void upgrade() {
 		if (!this.upgraded) {
 			upgradeName();
@@ -64,10 +65,10 @@ public class ShatteredReality extends AbstractShiftCard {
 	
 	public int damageCalculation(AbstractPlayer player, AbstractMonster monster, int damage) {
 		float tmp = damage;
-		for (AbstractPower p : player.powers) tmp = p.atDamageGive(tmp, DamageInfo.DamageType.NORMAL);
-		for (AbstractPower p : monster.powers) tmp = p.atDamageReceive(tmp, DamageInfo.DamageType.NORMAL);
-		for (AbstractPower p : player.powers) tmp = p.atDamageFinalGive(tmp, DamageInfo.DamageType.NORMAL);
-		for (AbstractPower p : monster.powers) tmp = p.atDamageFinalReceive(tmp, DamageInfo.DamageType.NORMAL);
+		for (AbstractPower p : player.powers) tmp = p.atDamageGive(tmp, this.damageTypeForTurn);
+		for (AbstractPower p : monster.powers) tmp = p.atDamageReceive(tmp, this.damageTypeForTurn);
+		for (AbstractPower p : player.powers) tmp = p.atDamageFinalGive(tmp, this.damageTypeForTurn);
+		for (AbstractPower p : monster.powers) tmp = p.atDamageFinalReceive(tmp, this.damageTypeForTurn);
 		int output = MathUtils.floor(tmp);
 		if (output < 0) output = 0;
 		return output;
